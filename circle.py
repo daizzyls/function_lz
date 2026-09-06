710fbdf374f6bbd993f872ffb523b0bf01d7dd43
DAIZZY_LS <daniklap2008@gmail.com>
2026-09-06 18:32:58 +0300

    Create circle.py

---
+++ circle.py
@@ -0,0 +1,10 @@
+import math
+
+PI = 3.1415
+
+r = float(input("Введите радиус окружности: "))
+area = PI * r**2
+circumference = 2 * PI * r
+
+print(f"Площадь круга: {area:.2f}")
+print(f"Длина окружности: {circumference:.2f}")
